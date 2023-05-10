4c8fdfcef0a7ce842339dad14c7b805645e91a4c
jewelrana3 <juyelrana7752@gmail.com>
2023-05-10 14:54:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+const [user,setUser] = useState([])
+
+useEffect(()=>{
+  fetch('http://localhost:5000/users')
+  .then(res=>res.json())
+  .then(data=>setUser(data))
+},[])
+
+const handleUser=event=>{
+  event.preventDefault();
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const user = {name,email}
+  console.log(user)
+
+  fetch('http://localhost:5000/users',{
+    method:'POST',
+    headers:{
+      "content-type":"application/json",
+    },
+    body:JSON.stringify(user)
+  })
+  .then(res=>res.json())
+  .then(data=>{
+    console.log('client side',data)
+
+   const newUser = [...users,data]
+   setUser(newUser)
+
+    form.reset()
+  })
+}
+  return (
+    <>
+     <h1>User client side is vary danger</h1>
+     <h2>User number :{user.length}</h2>
+      <form onSubmit={handleUser}>
+        <input type="text" name="name" id="n" /><br />
+        <input type="email" name="email" id="e" /><br />
+        <input type="submit" value="Add User" />
+      </form>
+    </>
+  )
+}
+
+export default App
